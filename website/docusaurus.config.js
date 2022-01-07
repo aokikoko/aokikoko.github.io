71151bd755c5cf8539b42b3e9e9f260ab9ca27e2
aokikoko <wanght@lenovocloud.com>
2022-01-07 22:51:22 +0800

    feat: github action测试

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'My Site啊啊',
   tagline: 'Dinosaurs are cool',
   url: 'https://phero.live',
   baseUrl: '/',
