e1aafdb8362972fa73637c86b4032d9a944d6f65
aokikoko <wanght@lenovocloud.com>
2022-01-10 21:12:34 +0800

    feat: 添加遮罩层前

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,58 +1,79 @@
-import React from "react";
-import clsx from "clsx";
+import React, { useState, useRef } from "react";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
-import HomepageFeatures from "../components/HomepageFeatures";
-import mp4 from "../../static/Ep01_HomeTeaser.mp4";
-import pic from "../../static/img/ski.png";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import mp4 from "../../static/index-page/Ep01_HomeTeaser.mp4";
+import pic from "../../static/index-page/ski.png";
+import Pause from "../../static/index-page/pause-svgrepo-com.svg";
+import Play from "../../static/index-page/play-svgrepo-com.svg";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+  const videoRef = useRef(null);
+  const [videoPlay, setVideoPlay] = useState(true);
+
+  const pauseVideo = () => {
+    videoRef.current.pause();
+    setVideoPlay(false);
+  };
+
+  const playVideo = () => {
+    videoRef.current.play();
+    setVideoPlay(true);
+  };
+
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <div className={styles.slider_container}>
-        <div className={styles.slider_pic}>
-          <img src={pic} className={styles.slider_img}></img>
+      <div className={styles.section_a}>
+        <div className={styles.section_b}>
+          <figure>
+            <img src={pic} className={styles.section_img}></img>
+          </figure>
         </div>
-        <div className={styles.slider_mp4}>
-          <video autoPlay loop muted>
+        <div className={styles.section_mp4}>
+          <video
+            autoPlay
+            loop
+            muted
+            type="video/mp4"
+            className={styles.section_mp5}
+            ref={videoRef}
+          >
             <source src={mp4} type="video/mp4" />
           </video>
         </div>
-        <div className={styles.slider_content}>
-          <h2>Aspects:Ep.01</h2>
-          <p>
-            Excitement drives us to explore. But what about when it overwhelms
-            us? Experienced Swiss freeriders share what happened when what
-            should have been the perfect day ended in an avalanche rescue.
-          </p>
+      </div>
+      <div className={styles.test_a}>
+        <div className={styles.test_b}>
+          <h2 className={styles.test_c} aria-label="Test">
+            <div className={styles.test_Aspects}>Aspects:&nbsp;</div>
+            <div>Ep.&nbsp;</div>
+            <div>01</div>
+          </h2>
+          <div>
+            <p className={styles.test_p}>
+              Excitement drives us to explore. But what about when it overwhelms
+              us? Experienced Swiss freeriders share what happened when what
+              should have been the perfect day ended in an avalanche rescue.
+            </p>
+          </div>
         </div>
       </div>
+      {/* 暂停|播放按钮 */}
+      {videoPlay ? (
+        <Pause
+          className={styles.pause}
+          onClick={() => pauseVideo()}
+        ></Pause>
+      ) : (
+        <Play
+          className={styles.play}
+          onClick={() => playVideo()}
+        ></Play>
+      )}
     </Layout>
   );
 }
